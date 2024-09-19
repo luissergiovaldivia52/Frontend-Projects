dc6d58e95ad991166cf2e5fec5a66a33d53e4360
luissergiovaldivia52 <luis_sergio_valdivia@hotmail.com>
2024-09-19 10:58:48 -0300

    Page Content

--- SinglePageCv/src/App.jsx
+++ SinglePageCv/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import PageContent from "./Component/PageContent";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,7 @@ function App() {
     <>
       <div>
         <main>
-          <p className="read-the-docs">Hola Mundo</p>
+          <PageContent />
         </main>
       </div>
     </>
