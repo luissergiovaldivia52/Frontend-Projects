2bcb40e91622e3ef3113a0c2b403cc7db7c07690
luissergiovaldivia52 <luis_sergio_valdivia@hotmail.com>
2024-09-20 10:10:30 -0300

    single page cv page content

--- SinglePageCv/src/Component/PageContent.jsx
+++ SinglePageCv/src/Component/PageContent.jsx
@@ -3,7 +3,43 @@ import React from "react";
 export default function PageContent() {
   return (
     <div>
-      <p>Your Name</p>
+      <div>
+        <p>Your Name</p>
+      </div>
+      <div>
+        <p>Junior Frontend Developer</p>
+        <p>123 Your Street</p>
+        <p>You City, ST 12345</p>
+        <p>(123)456-7890</p>
+        <p>no_reply@example.com</p>
+      </div>
+      <div>
+        <p>Skills</p>
+        <p>
+          HTML, CSS, Javascript, Accessibility, Figma to Disign, Responsive Web
+          Design,
+        </p>
+        <p>Technical Writing, Presentation</p>
+      </div>
+      <div>
+        <p>Education</p>
+        <p>School Name, Location - Degree</p>
+        <p>Month 20xx to Month 20xx</p>
+        <p>List of exciting thing you did at univerisity</p>
+      </div>
+      <div>
+        <p>Experience</p>
+        <p>Company Name, Location - Job Title</p>
+        <p>Month 20xx to Month 20xx</p>
+        <p>List of achievements</p>
+        <p>List of achievements</p>
+        <p>List of achievements</p>
+        <p>Skills: List of skills used or gained at this comapany</p>
+      </div>
+      <div>
+        <p>Across the internet</p>
+        <p>Add your Linkedln, GitHub profile links</p>
+      </div>
     </div>
   );
 }
